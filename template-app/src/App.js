4d4643ff263eeaa2dcce7b420c7ac66c4097c98b
Maji30an <majid.bahramyan@gmail.com>
2022-07-31 00:44:36 +0430

    edit files

--- template-app/src/App.js
+++ template-app/src/App.js
@@ -7,7 +7,13 @@ function App() {
      <>
         <Header />
         <main className='3xl:container mx-auto h-screen'>
-            <Customers />
+           <Customers />
+           <div className='text-center'>
+              <span className='text-[#CB0F4C] font-[Poppins] font-medium text-[28px] leading-[35px] tracking-[0.045em]'>Services</span>
+              <h3 className='font-bold text-5xl leading-[60px] tracking-[0.045em]'>What We Offer</h3>
+              <p className='text-[#362424] font-normal text-2xl leading-[30px] tracking-[0.045em]'>Ea nam dicat utamur tincidunt, cu sea audiam vocibus. Esse nemore mei eu.
+                 Virtute commune dissentiet te pri, te sonet periculis mel.</p>
+           </div>
         </main>
      </>
   );
