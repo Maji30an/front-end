4d238c45b47db954d393b0031ed4231febca5a5b
Maji30an <majid.bahramyan@gmail.com>
2022-08-02 07:56:55 +0430

    add and edit files

---
+++ template-app/src/components/Slide.js
@@ -0,0 +1,22 @@
+import React from "react";
+import arrowRight from "../assets/img/arrow-right.svg";
+import share from "../assets/img/share.svg";
+
+const Slide = (props) => {
+   return(
+      <div className={`${props.class} bg-no-repeat max-w-[520px]`}>
+         <div className='text-white py-10 px-[30px] bg-[#00000099]'>
+            <small className='font-poppins font-normal text-2xl tracking-45'>August 2, 2021</small>
+            <h4 className='font-poppins font-semibold text-[32px] leading-10 tracking-45 mt-[30px] mb-[100px]'>How to Uncrease Your ROI
+               Through scientific SEM?</h4>
+            <div className='flex gap-4'>
+               <a href='#' className='font-poppins font-normal text-2xl tracking-45'>READ MORE</a>
+               <img src={arrowRight} alt='arrow' />
+               <img src={share} alt='share' className='ml-auto' />
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Slide;
--- template-app/tailwind.config.js
+++ template-app/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         primary: '#CB0F4C',
         secondary: '#362424',
         third: '#191234',
+        dot: '#C4C4C4',
       },
       screens: {
         '3xl': '1640px'
@@ -21,6 +22,11 @@ module.exports = {
       boxShadow: {
         my: '0px 10px 40px rgba(0, 0, 0, 0.1)',
         us: '5px 5px 25px rgba(0, 0, 0, 0.08)',
+      },
+      backgroundImage: {
+        'blog-pic-1': "url('/src/assets/img/pic-1.svg')",
+        'blog-pic-2': "url('/src/assets/img/pic-2.svg')",
+        'blog-pic-3': "url('/src/assets/img/pic-3.svg')",
       },
     },
   },
